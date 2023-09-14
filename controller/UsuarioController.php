bb2fb4f0bcc1b7891af610548cc134d8acbaa0e8
Marcelo Santos <marcelo.santos@poder360.com.br>
2023-09-13 22:31:45 -0300

    Adicionando Usuario Controller

---
+++ controller/UsuarioController.php
@@ -0,0 +1,29 @@
+<?php
+//instanciar as classes
+
+$usuario = new Usuario();
+$usuariodao = new UsuarioDAO();
+
+//passar os posts - dados
+
+$d = filter_input_array(INPUT_POST);
+
+// Se for gravado com sucesso
+if(isset($_POST['cadastrar'])){
+
+    $usuario->setNome($d['nome']);
+    $usuario->setSobrenome($d['sobrenome']);
+    $usuario->setIdade($d['idade']);
+    $usuario->setSexo($d['sexo']);
+
+    $usuariodao->create($usuario);
+
+}
+
+//se for deleter
+
+else if(isset($_GET['del'])){
+
+    $usuario-setId($_GET['del']);
+    $usuariodao->delete($usuario);
+};
