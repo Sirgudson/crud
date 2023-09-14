85dfd7aa1f54419be7d49246ec8a8f4bd1938387
Marcelo Santos <marksantosdf@gmail.com>
2023-09-13 22:27:15 -0300

    Update UsuarioDAO.php

--- dao/UsuarioDAO.php
+++ dao/UsuarioDAO.php
@@ -22,9 +22,43 @@ class UsuarioDAO{
 
     public function read(){
         try {
-            $sql = "SELECT * FROM usuario order by nome asc"
-        } catch (\Throwable $th) {
-            //throw $th;
+            $sql = "SELECT * FROM usuario order by nome asc";
+            $result = Conexao::getConexao()->query($sql);
+            $result = $result->fetchAll(PDO::FETCH_ASSOC);
+            $f_lista = array();
+            foreach ($lista as $l) {
+                $f_lista[] = $this->listaUsuarios($l);
+            }
+
+            return $f_lista;
+        } catch (Exception $e) {
+            print "Erro ao buscar usuário <br>" .$e. '<br>';
         }
     }
+    public function update(Usuario $usuario){
+        try{
+            $sql = "UPDATE usuario SET
+            
+                nome =:nome,
+                sobrenome =:sobrenome,
+                idade =:idade,
+                sexo =:sexo
+                
+                WHERE id =:id";
+            $p_sql = Conexao:getConexao()->prepare($sql);
+            $p_sql ->bindValue(":nome",$usuario->getNome());
+            $p_sql ->bindValue(":sobrenome",$usuario->getSobrenome());
+            $p_sql ->bindValue(":idade",$usuario->getIdade());
+            $p_sql ->bindValue(":sexo",$usuario->getSexo());
+            $p_sql ->bindValue(":id",$usuario->getId()); 
+            
+            return $p_sql->execute();
+
+        }
+        catch (Exception $e){
+            print "Erro ao atualizar usuário <br>" .$e. '<br>';
+        }
+    }
+
+    
 }
